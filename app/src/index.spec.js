4a798b67dafa7285d853799a1c280a27263c89dc
jp6rt <jp01r7@gmail.com>
2018-03-17 21:20:32 +0800

    Initial commit+tests

---
+++ app/src/index.spec.js
@@ -0,0 +1,9 @@
+describe('Jasmine sanity', () => {
+	it('should return an error', () => {
+		expect(1+1).toBe(3)
+	})
+
+	it('should NOT return an error', () => {
+		expect(5).toBeGreaterThan(4.9)
+	})
+})
