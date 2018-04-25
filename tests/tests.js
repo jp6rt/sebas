ba614b6c376641f56c0bbbf74b22760fb2dd2589
jp6rt <jp01r7@gmail.com>
2018-04-25 23:34:48 +0800

    +subview test

--- tests/tests.js
+++ tests/tests.js
@@ -44,6 +44,17 @@ const { format } = require('@jp6rt/utils')
 			response.end()
 
 		})
+	
+	sebas.get('/view/sub')
+		.pipe((request, response) => {
+
+			response.writeHead(200, {
+				'Content-Type': 'text/html'
+			})
+
+			response.end('Hello /view/sub')
+
+		})
 
 	await sebas.start({ debugMode: !0, port: 3000 })
 
@@ -77,6 +88,14 @@ const { format } = require('@jp6rt/utils')
 		const { headers }= response
 
 		logger.silent('response headers: {0}', headers)
+
+		response.on('data', (data) => {
+			logger.silent('response data: {0}', data.toString())
+		})
+
+	})
+
+	http.get('http://localhost:3000/view/sub', (response) => {
 
 		response.on('data', (data) => {
 			logger.silent('response data: {0}', data.toString())
